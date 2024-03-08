607e06a44c22a11b7bc23205d6abdda1a42c397b
erhic <ericngugi24@gmail.com>
2024-03-08 12:09:46 +0300

    feat : create a js file to export constant data

---
+++ src/constants/index.js
@@ -0,0 +1,26 @@
+export const navLinks = [
+  { href: "/", label: "Home" },
+  { href: "/product", label: "Product" },
+  { href: "/aboutus", label: "About Us" },
+  { href: "/services", label: "Services" },
+  { href: "/blog", label: "Blog" },
+  { href: "/contact", label: "Contact" },
+];
+
+export const heroBtn = [{ label: "Order Pickup", href: "", btnIcon: "" }];
+
+export const onboard = [
+  {
+    step: "Account Registration",
+    description:
+      " We value your identity ,help us know who , where you need this service",
+  },
+  {
+    step: "Request Service",
+    description: " You request ,we deliver our services at your door step",
+  },
+  {
+    step: "Order Pickup",
+    description: "We are standby waiting for you to click that button .",
+  },
+];
