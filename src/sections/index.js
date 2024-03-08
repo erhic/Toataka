d100e67a44c68e42feabd188398acce3bc93dad2
erhic <ericngugi24@gmail.com>
2024-03-08 12:10:43 +0300

    feat : create a file to export components from single origin, to keep clean code

---
+++ src/sections/index.js
@@ -0,0 +1,8 @@
+import Hero from "./Hero";
+import About from "./About";
+import Product from "./Product";
+import Services from "./Services";
+import Contact from "./Contact";
+import Footer from "./Footer";
+import Blog from "./Blog";
+export { Hero, About, Product, Services, Contact, Footer, Blog };
