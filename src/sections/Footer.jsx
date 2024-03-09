c87dd5a05caed9e0737e2a501fbf74155771e80f
erhic <ericngugi24@gmail.com>
2024-03-09 23:46:13 +0300

    feat : create footer section

---
+++ src/sections/Footer.jsx
@@ -0,0 +1,17 @@
+
+const Footer = () => {
+  return (
+    <section className="flex flex-row gap-10 justify-center bg-green-900 text-white font-palanquin text-base pb-10">
+      <div className="flex gap-10 flex-row max-lg:flex-col">
+        <p> Copyright @ 2024 </p>
+        <p> All rights reserved </p>
+      </div>
+      <div className="flex gap-10 flex-row max-lg:flex-col">
+        <p> Terms & Condition </p>
+        <p> Privacy Policy </p>
+      </div>
+    </section>
+  )
+}
+
+export default Footer
