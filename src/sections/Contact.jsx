5cdc416d48bc547804b2e3f4d11c2c9422ab31f5
erhic <ericngugi24@gmail.com>
2024-03-09 23:45:37 +0300

    feat: create contact page

---
+++ src/sections/Contact.jsx
@@ -0,0 +1,37 @@
+
+const Contact = () => {
+  return (
+    <section className="flex flex-row max-lg:flex-col gap-10 justify-between px-16 bg-green-900 text-white py-16 font-palanquin ">
+      <div className="max-w-[200px]">
+        <h2 className="text-lg font-semibold pb-3">About Us</h2>
+
+        <p>Factories can’t be recycling, which causes pollution.Industries do not focus on recycling; they concentrate only on the raw material.</p>
+        <p></p>
+      </div>
+      <div>
+        <h2 className="text-lg font-semibold pb-3">Quick Links</h2>
+        <p>About Us </p>
+        <p>Services</p>
+        <p>Request Pickup</p>
+        <p>Contact Us</p>
+      </div>
+      <div>
+        <h2 className="text-lg font-semibold pb-3">Working Hours</h2>
+        <p>We are all available at our offices within the week</p>
+        <p>Mon - Fri: 9:00AM - 6:00PM</p>
+        <p>Sat - Sun: 8:00AM - 4:00PM</p>
+        <p></p>
+      </div>
+
+      <div>
+        <h2 className="text-lg font-semibold pb-3">Get in Touch</h2>
+        <p>North Airport Road , Embakasi</p>
+        <p>Nairobi</p>
+        <p> (+254) 790 999 999</p>
+        <p> customercare@toataka.com</p>
+      </div>
+    </section>
+  )
+}
+
+export default Contact
