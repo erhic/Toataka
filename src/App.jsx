48a947692aa14e36e4bbf8a9f085199bb3f59156
erhic <ericngugi24@gmail.com>
2024-03-10 01:49:33 +0300

    feat: added our stories section on works component

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import Nav from "./Components/Nav"
+import { Hero, About, Footer, Product, Blog, Services, Works } from "./sections"
+import Contact from "./sections/Contact"
+
+export default function App() {
+  return (
+    <main>
+      <section><Nav /> </section>
+      <section className="xl:padding-l px-16 wide:padding-r padding-b"><Hero /> </section>
+      <section className="padding"><About /> </section>
+      <section className="padding"><Services />
+      </section> <section className="padding-x py-10"><Product /> </section>
+      <section className="padding bg-pale-blue"><Blog /> </section>
+      <section className="padding "><Works /> </section>
+      <section className=""><Contact /></section>
+      <section className=""><Footer /></section>
+
+    </main>
+  )
+}
