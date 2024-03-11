2d769c76921f0d5c152e1d26ea5189b550490427
erhic <ericngugi24@gmail.com>
2024-03-12 00:11:13 +0300

    feat :  create register form

--- toatakaFrontend/src/Components/Register.jsx
+++ toatakaFrontend/src/Components/Register.jsx
@@ -2,9 +2,9 @@ import { dustbin } from "../assets/images"
 
 const Register = () => {
   return (
-    <div className="border flex justify-center rounded max-h-[405px] ">
+    <div className=" flex justify-center rounded max-h-[405px] ">
       <div className="max-w-[328px] rounded-tl-lg overflow-hidden rounded-bl-lg max-lg:hidden"><img src={dustbin} alt="" /></div>
-      <form action="" className=" gap-5 flex flex-col p-6 border border-green-900 rounded-tr-lg  rounded-br-lg max-lg:rounded-lg">
+      <form action="" className=" gap-5 flex flex-col p-6  border  rounded-tr-lg  rounded-br-lg max-lg:rounded-lg hover:border-green-800">
         <h2 className="flex  justify-center font-montserrat font-bold text-green-900 text-lg">Register</h2>
         <input className="border px-3 py-3 rounded-lg" type="text" placeholder="Enter username " />
         <input className="border px-3 py-3 rounded-lg" type="text" placeholder="Enter email " />
