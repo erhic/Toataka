50dff400c38e894122b7a674edaae3012ab68f7b
erhic <ericngugi24@gmail.com>
2024-03-16 13:22:21 +0300

    feat: set up route to our server ,to perform request

---
+++ toatakaBackend/app.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+app.post("register", (req, res) => {});
+app.post("login", (req, res) => {});
+app.post("orderservice", (req, res) => {});
