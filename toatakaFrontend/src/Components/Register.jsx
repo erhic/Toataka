5c2086c41605133e370a3e0e3a918f48c4f65085
erhic <ericngugi24@gmail.com>
2024-03-16 03:46:06 +0300

    feat : set up variable to share registration details to the server

--- toatakaFrontend/src/Components/Register.jsx
+++ toatakaFrontend/src/Components/Register.jsx
@@ -1,17 +1,27 @@
+import { useState } from "react"
 import { dustbin } from "../assets/images"
 
 const Register = () => {
+  const [username, setUserName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassWord] = useState('')
+  const [confirmpassword, setConfirmPassword] = useState('')
+
+  const handleRegister = () => {
+
+  }
+
   return (
     <div className=" flex justify-center rounded max-h-[405px] ">
       <div className="max-w-[328px] rounded-tl-lg overflow-hidden rounded-bl-lg max-lg:hidden"><img src={dustbin} alt="" /></div>
       <form action="" className=" gap-5 flex flex-col p-6  border  rounded-tr-lg  rounded-br-lg max-lg:rounded-lg hover:border-green-800">
         <h2 className="flex  justify-center font-montserrat font-bold text-green-900 text-lg">Register</h2>
-        <input className="border px-3 py-3 rounded-lg" type="text" placeholder="Enter username " />
-        <input className="border px-3 py-3 rounded-lg" type="text" placeholder="Enter email " />
-        <input className="border px-3 py-3 rounded-lg" type="password" placeholder="Enter password" />
-        <input className="border px-3 py-3 rounded-lg" type="password" placeholder="Confirm password" />
+        <input className="border px-3 py-3 rounded-lg" value={username} onChange={setUserName} type="text" placeholder="Enter username " />
+        <input className="border px-3 py-3 rounded-lg" value={email} onChange={setEmail} type="text" placeholder="Enter email " />
+        <input className="border px-3 py-3 rounded-lg" value={password} onChange={setPassWord} type="password" placeholder="Enter password" />
+        <input className="border px-3 py-3 rounded-lg" value={confirmpassword} onChange={setConfirmPassword} type="password" placeholder="Confirm password" />
         <div className="flex justify-center ">
-          <button className="bg-green-900 px-6 py-2 rounded-full text-white max-w-[120px]">
+          <button className="bg-green-900 px-6 py-2 rounded-full text-white max-w-[120px]" onClick={handleRegister}>
             Register
           </button>
         </div>
